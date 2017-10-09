fc2e0999fc4eb98a7a537cca0a0c1999903d8430
gaiyulong <gyl@acewill.cn>
2017-10-10 00:46:53 +0800

    'eat'

--- eat/js/background.js
+++ eat/js/background.js
@@ -1,4 +1,32 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('popup.html',
-    {id: 'eat', innerBounds: {width: 800, height: 550}});
+chrome.browserAction.setBadgeText({text: 'new'});
+chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]});
+
+chrome.contextMenus.create({
+	title: "吃啥",
+	onclick: function(){alert('吃啥');}
+});
+chrome.notifications.create(null, {
+	type: 'basic',
+	iconUrl: 'eat.png',
+	title: '这是标题',
+	message: '您刚才点击了自定义右键菜单！'
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'g' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
 });
